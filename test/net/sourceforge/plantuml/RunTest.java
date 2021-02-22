e99b4cd4f3601ce051bdf214e3a7c5ece97c44e4
Yasuyuki ENDO <eyasuyuki@gmail.com>
2021-02-23 06:31:51 +0900

    add maven-sufire-plugin configuration, test file

--- test/net/sourceforge/plantuml/RunTest.java
+++ test/net/sourceforge/plantuml/RunTest.java
@@ -3,9 +3,13 @@ package net.sourceforge.plantuml;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 class RunTest {
+    public static String[] TEST_FILES = {"address.puml"};
+
     @Test
-    public void runTest() {
-        System.out.println("Hello from test main.");
+    public void runTest() throws IOException, InterruptedException {
+        Run.main(TEST_FILES);
     }
 }
