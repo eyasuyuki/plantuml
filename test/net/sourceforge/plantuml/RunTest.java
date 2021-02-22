54e0017197cc84ca30f69514f522b36f45fa7898
Yasuyuki ENDO <eyasuyuki@gmail.com>
2021-02-23 06:00:51 +0900

    add JUnit5 test

---
+++ test/net/sourceforge/plantuml/RunTest.java
@@ -0,0 +1,11 @@
+package org.javaopen.plantuml;
+
+
+import org.junit.jupiter.api.Test;
+
+class RunTest {
+    @Test
+    public void runTest() {
+        System.out.println("Hello from test main.");
+    }
+}
