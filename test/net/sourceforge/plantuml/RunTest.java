124e3216a9bc32d2eae925fd4b0368d35d0a5279
Yasuyuki ENDO <eyasuyuki@gmail.com>
2021-02-25 18:05:39 +0900

    add RunTest

--- test/net/sourceforge/plantuml/RunTest.java
+++ test/net/sourceforge/plantuml/RunTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 class RunTest {
-    public static String[] TEST_FILES = {"address.puml"};
+    public static String[] TEST_FILES = {"src/test/resources/address.puml"};
 
     @Test
     public void runTest() throws IOException, InterruptedException {
+        System.out.println(System.getProperty("user.dir"));
         Run.main(TEST_FILES);
     }
 }
